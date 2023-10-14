2deb1f541b1eb61904d93bec2116b6746dbeb64a
Avishka Chanaka <It206429142@my.sliit.lk>
2023-10-14 10:39:42 +0530

    fixed

--- BookMySeat/BookMySeat/Controllers/TravelerController.cs
+++ BookMySeat/BookMySeat/Controllers/TravelerController.cs
@@ -2,10 +2,11 @@
 using BookMySeat.Models;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace BookMySeat.Controllers
 {
+    /// <summary>
+    /// Controller for managing traveler-related operations in the BookMySeat application.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class TravelerController : ControllerBase
@@ -17,27 +18,32 @@ namespace BookMySeat.Controllers
             this.TravelerService = TravelerService;
         }
 
-
-        // GET: api/<TravelerController>
+        /// <summary>
+        /// Retrieves a list of all travelers.
+        /// </summary>
         [HttpGet]
         public ActionResult<List<Traveler>> Get()
         {
             return TravelerService.Get();
         }
 
-        // GET api/<TravelerController>/5
+        /// <summary>
+        /// Retrieves traveler details by its unique identifier.
+        /// </summary>
         [HttpGet("getTravelerById")]
         public ActionResult<Traveler> Get(string id)
         {
             var traveler = TravelerService.Get(id);
             if (traveler == null)
             {
-                return NotFound($" Traveler with ID={id} not found");
+                return NotFound($"Traveler with ID={id} not found");
             }
             return traveler;
         }
 
-        // POST api/<TravelerController>
+        /// <summary>
+        /// Creates a new traveler record.
+        /// </summary>
         [HttpPost]
         public ActionResult<Traveler> CreateA([FromBody] Traveler traveler)
         {
@@ -45,29 +51,33 @@ namespace BookMySeat.Controllers
             return CreatedAtAction(nameof(Get), new { id = traveler.TravelerId }, traveler);
         }
 
-        // PUT api/<TravelerController>/5
+        /// <summary>
+        /// Updates an existing traveler record by its ID.
+        /// </summary>
         [HttpPut("UpdateTravelerById")]
         public ActionResult Put(string id, [FromBody] Traveler traveler)
         {
-            var exisitingTraveler = TravelerService.Get(id);
+            var existingTraveler = TravelerService.Get(id);
 
-            if (exisitingTraveler == null)
+            if (existingTraveler == null)
             {
-                return NotFound($" Traveler with ID={id} not found");
+                return NotFound($"Traveler with ID={id} not found");
             }
             TravelerService.Update(id, traveler);
             return NoContent();
         }
 
-        // DELETE api/<TravelerController>/5
+        /// <summary>
+        /// Removes a traveler record by its ID.
+        /// </summary>
         [HttpDelete("deleteTravelerById")]
         public ActionResult Delete(string id)
         {
-            var Traveler = TravelerService.Get(id);
+            var traveler = TravelerService.Get(id);
 
-            if (Traveler == null)
+            if (traveler == null)
             {
-                return NotFound($" Traveler with ID={id} not found");
+                return NotFound($"Traveler with ID={id} not found");
             }
             TravelerService.Remove(id);
 
